6f761397db1eb6941293c742f6ac34d1c6b12348
hdussert <hdussert@student.42.fr>
2019-05-03 18:04:25 +0200

    Dont merge

--- includes/doom.h
+++ includes/doom.h
@@ -115,6 +115,8 @@ typedef struct	s_settings
 	int			key_right;
 	int			key_sprint;
 	int			key_crouch;
+	int			z_up;
+	int			z_down;
 	int			render_textures;
 	int			enable_crt_floor;
 }				t_settings;
@@ -296,7 +298,6 @@ typedef struct		s_player
 	double			anglesin;
 	double			pitch;
 	double			speed;
-	double			rotspeed;
 	int				rov;
 	int				is_sprinting;
 	int				is_crouching;
@@ -319,6 +320,8 @@ typedef struct		s_key
 	int		right;
 	int		up;
 	int		down;
+	int		z_up;
+	int		z_down;
 	int		shadow;
 }					t_key;
 
@@ -353,6 +356,7 @@ typedef struct		s_raycasting
 {
 	SDL_Surface	*texture;
 	int			x;
+	double		p_z;
 	double		camera_x;
 	t_vec		ray_dir;
 	t_vec_int	map;
@@ -501,8 +505,8 @@ SDL_Rect			make_rect(int x, int y, int width, int height);
 void				draw_rect(SDL_Surface *s, SDL_Rect rect, SDL_Color color, int fill_rect);
 void				draw_rect_u(SDL_Surface *s, SDL_Rect rect, Uint32 color, int fill_rect);
 
-void				turn(double angle, t_player *you, t_doom *doom);
-void				moove(double dist, t_doom *doom, t_map *map, int ang);
+void				turn(double angle, t_player *you);
+void				moove(double dist, t_player *player, t_map *map, double ang);
 
 void				ed_bt_edit_click(t_doom *doom, t_el_button *b, SDL_MouseButtonEvent ev);
 
