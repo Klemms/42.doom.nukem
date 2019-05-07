8be68210565c5c66dadb940453521d8e408ab9e8
Laurent DE BATZ DE TRENQUELLEON <lde-batz@e2r10p11.42.fr>
2019-05-08 00:19:51 +0200

    open door no key

--- src/button.c
+++ src/button.c
@@ -6,7 +6,7 @@
 /*   By: lde-batz <lde-batz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/06 19:46:06 by lde-batz          #+#    #+#             */
-/*   Updated: 2019/05/07 23:45:39 by lde-batz         ###   ########.fr       */
+/*   Updated: 2019/05/08 00:18:29 by lde-batz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,9 +37,20 @@ void	check_event_id(t_doom *doom, int pos_x, int pos_y)
 	}
 }
 
+void	check_event_id(t_doom *doom, int pos_x, int pos_y)
+{
+	doom->nmap->map[i][j].collides = 0;
+	doom->nmap->map[i][j].state = 1;
+}
+
 void	press_button2(t_doom *doom, int *pos_x, int *pos_y, int *orientation)
 {
-	if (doom->you.angle < M_PI_4)
+	if (doom->you.angle < N_PI_4)
+	{
+		pos_y -= 1;
+		orientation = 1;
+	}
+	else if (doom->you.angle < M_PI_4)
 	{
 		*pos_x += 1;
 		*orientation = 0;
@@ -59,15 +70,10 @@ void	press_button(t_doom *doom)
 
 	pos_x = (int)doom->you.pos.x;
 	pos_y = (int)doom->you.pos.y;
-	if (doom->you.angle < N_3_PI_4 && doom->you.angle >= P_3_PI_4)
+	if (doom->you.angle < N_3_PI_4 || doom->you.angle >= P_3_PI_4)
 	{
 		pos_x -= 1;
 		orientation = 0;
-	}
-	else if (doom->you.angle < N_PI_4)
-	{
-		pos_y -= 1;
-		orientation = 1;
 	}
 	else
 		press_button2(doom, &pos_x, &pos_y, &orientation);
@@ -75,4 +81,11 @@ void	press_button(t_doom *doom)
 	&& doom->nmap->map[pos_y][pos_x].orientation == orientation
 	&& !doom->nmap->map[pos_y][pos_x].state && doom->you.hud.key > 0)
 		check_event_id(doom, pos_x, pos_y);
+	else if (doom->nmap->map[pos_y][pos_x].block_type == block_door
+	&& doom->nmap->map[pos_y][pos_x].orientation == orientation
+	&& doom->nmap->map[pos_y][pos_x].event_id >= 100)
+	{
+		doom->nmap->map[i][j].collides = 0;
+		doom->nmap->map[i][j].state = 1;
+	}
 }
